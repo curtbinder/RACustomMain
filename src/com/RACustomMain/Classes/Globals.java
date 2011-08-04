f262f912968f240f2e4e34f7cf0afd6bf8e32741
dmolton <fossil5000@gmail.com>
2011-08-03 22:22:55 -0400

    Committing base project.

---
+++ src/com/RACustomMain/Classes/Globals.java
@@ -0,0 +1,24 @@
+package com.RACustomMain.Classes;
+
+import java.awt.Color;
+import java.awt.Font;
+
+
+public class Globals {
+	
+	
+	public static final int HugeControlWidth = 66;
+	public static final int HugeControlHeight = 18;
+	public static final Color HugeControlBGColor = Color.WHITE;
+	public static final Font HugeControllerFont = new Font("Courier New", Font.BOLD, 12);
+	
+	public static final int LargeControlWidth = 46;
+	public static final int LargeControlHeight = 18;
+	public static final Color LargeControlBGColor = Color.WHITE;
+	public static final Font LargeControllerFont = new Font("Courier New", Font.BOLD, 12);
+	
+	public static final int NormalControlWidth = 26;
+	public static final int NormalControlHeight = 10;
+	public static final Color NormalControlBGColor = Color.WHITE;
+	public static final Font NormalControllerFont = new Font("Courier New", Font.BOLD, 10);
+}
